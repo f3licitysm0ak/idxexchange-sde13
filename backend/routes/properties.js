855183c500bfad69ecf348bcacff68f4e641ec29
f3licitysm0ak <prachijmarfatia@gmail.com>
2026-07-08 15:55:06 -0700

    new version of properties api route

--- backend/routes/properties.js
+++ backend/routes/properties.js
@@ -6,7 +6,9 @@ app.get("/api/properties", async(req, res) => {
         let OFFSET = req.query.offset ? Number(req.query.offset) : 0;
 
         if (isNaN(LIMIT) || isNaN(OFFSET) || LIMIT <= 0 || LIMIT >= 200){
-            //res 400 status
+            return res.status(400).json({
+                message: "Invalid filter value"
+            });
         }
 
         const filtersMap = new Map([
@@ -27,30 +29,38 @@ app.get("/api/properties", async(req, res) => {
         
         if (req.query.minPrice) {
             if (isNaN(req.query.minPrice)) {
-                //res status 400
+                return res.status(400).json({
+                    message: "Invalid filter value"
+                });
             }
             whereClauses.push(`SystemPrice >= ${req.query.minPrice}`);
         }
 
         if (req.query.maxPrice) {
             if (isNaN(req.query.maxPrice)) {
-                //res status 400
+                return res.status(400).json({
+                    message: "Invalid filter value"
+                });
             }
             whereClauses.push(`SystemPrice <= ${req.query.maxPrice}`);
         }
 
         if (req.query.beds && isNaN(Number(req.query.beds))) {
-            //res status 400
+            return res.status(400).json({
+                message: "Invalid filter value"
+            });
         }
         if (req.query.baths && isNaN(Number(req.query.baths))) {
-            //res status 400
+            return res.status(400).json({
+                message: "Invalid filter value"
+            });
         }
 
         let whereClause = " WHERE " + whereClauses.join(" AND "); //added spaces so L_City = "Chicago" AND L_Zip = 83948 instead of L_City = "Chicago"ANDL_Zip = 83948 
         if (whereClauses.length === 0) {
             whereClause = "";
         }
-        let filteredQuery = `SELECT * FROM rets_property ${whereClause} ORDER BY id LIMIT ${LIMIT} OFFSET${OFFSET}`; //making sure the order is proper of SELECT then WHERE then ORDER BY
+        let filteredQuery = `SELECT * FROM rets_property ${whereClause} ORDER BY id LIMIT ${LIMIT} OFFSET ${OFFSET}`; //making sure the order is proper of SELECT then WHERE then ORDER BY
         
 
         const [properties] = await pool.query(filteredQuery);
@@ -67,7 +77,7 @@ app.get("/api/properties", async(req, res) => {
 
 
     } catch(err) {
-        res.status(500).json({
+        return res.status(500).json({
             status : "error",
             message : err.message
         });
